277abc409e033b31bafe866f98b55cb807ddf1e8
monawasensei <monawasensei@gmail.com>
2023-11-29 11:26:16 -0500

    Move the archive style sheet into the resources directory as well.

--- archive/gallery.php
+++ archive/gallery.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Gallery</title>
 
-  <link rel="stylesheet" href="archiveStyle.CSS">
+  <link rel="stylesheet" href="../resource/archiveStyle.CSS">
 </head>
 <body>
   <div class="gallery">
