cbed48a042de673ba17e41435072d860e3b833a9
monawasensei <monawasensei@gmail.com>
2023-12-01 11:15:11 -0500

    Merge the archive stylesheet into the normal stylesheet.

--- archive/gallery.php
+++ archive/gallery.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Gallery</title>
 
-  <link rel="stylesheet" href="../resource/archiveStyle.CSS">
+  <link rel="stylesheet" href="../resource/stylesheet.css">
 </head>
 <body>
   <div class="gallery">
